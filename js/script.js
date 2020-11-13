fa4f590eef3d13fe936eefa433b62a591e459b25
Cristian <cristianmihai193@gmail.com>
2020-11-13 16:03:45 +0100

    Step1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+calcola.addEventListener("click",
+function() {
+  var nome = document.getElementById("nome").value;
+  console.log(nome);
+  var km = document.getElementById("km").value;
+  console.log(km);
+  var eta = document.getElementById("eta").value;
+  console.log(eta);
+  }
+);
