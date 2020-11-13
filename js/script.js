91571bd0b5ce428042724d1fbe7f95ee575033de
Cristian <cristianmihai193@gmail.com>
2020-11-13 16:30:12 +0100

    Step2

--- js/script.js
+++ js/script.js
@@ -7,5 +7,34 @@ function() {
   console.log(km);
   var eta = document.getElementById("eta").value;
   console.log(eta);
+  // il prezzo del biglietto è definito in base ai km (0.21 € al km);
+  var costoBigliettoAlKm = 0.21;
+  console.log(costoBigliettoAlKm)
+
+  // prezzo totale del biglietto
+  var costoBigliettoTotale = km * costoBigliettoAlKm;
+  console.log("Costo Biglietto", costoBigliettoTotale)
+
+  // va applicato uno sconto del 20% per i minorenni;
+  var scontoMinorenni = ((costoBigliettoTotale * 20) / 100);
+  var prezzoScontatoMinorenni = (costoBigliettoTotale - scontoMinorenni);
+  console.log("Sconto Minorenni", scontoMinorenni)
+
+  // va applicato uno sconto del 40% per gli over 65.
+  var scontoOver = ((costoBigliettoTotale * 40) / 100);
+  var prezzoScontatoOver = (costoBigliettoTotale - scontoOver);
+  console.log("ScontoOver", scontoOver)
+
+
+  if (eta == "minorenne") {
+
+    console.log("Prezzo minorenni", costoBigliettoTotale - scontoMinorenni);
+  } else if (eta == "over65") {
+
+    console.log("Prezzo Over65", costoBigliettoTotale - scontoOver);
+  } else {
+
+    console.log("Prezzo normale", costoBigliettoTotale);
+  }
   }
 );
